da5b4392e2ed72a825bc03a287d5a7eea3855a15
Jason Plumb <jaymjplumb@gmail.com>
2015-06-02 12:07:10 -0700

    / with sed

--- sed/sedCommands.sh
+++ sed/sedCommands.sh
@@ -6,4 +6,6 @@ sed 's/this/change/g' test.sql
 # sed -n 's/FindThisWord/ReplaceWithThisWord/gpw output.txt'  input.txt
 sed -n 's/hi/herro/gpw output.txt'  test.txt
 
-
+# Replace a single word with / at the end
+# sed -n "s|FindThisWord/|RepaceWithThisWord|gpw output.txt"  test.txt
+# sed -n "s|hello/|dfd|gpw output.txt"  test.txt
